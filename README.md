8207ccaf066f67b53ee48c16d8ff864c7b5bd6c8
Matt Price <matt.price@utoronto.ca>
2024-04-23 12:28:58 -0400

    first real commit: rename to ox-myst, implement some features

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
-[![MELPA](http://melpa.org/packages/ox-gfm-badge.svg)](http://melpa.org/#/ox-gfm)
-# Github Flavored Markdown exporter for Org Mode
+[![MELPA](http://melpa.org/packages/ox-myst-badge.svg)](http://melpa.org/#/ox-myst)
+# Myst Markdown exporter for Org Mode
 
 This is a small exporter based on the Markdown exporter already existing in
 Org mode. It should support the features [listed here](https://help.github.com/articles/github-flavored-markdown/).
 
 ## Installation
 
-You can install `ox-gfm` using elpa. It's available on [melpa](http://melpa.org/#/ox-gfm):
+You can install `ox-myst` using elpa. It's available on [melpa](http://melpa.org/#/ox-myst):
 
-<kbd> M-x package-install ox-gfm </kbd>
+<kbd> M-x package-install ox-myst </kbd>
 
 ## Usage
 
 This package adds an Org mode export backend for GitHub Flavored Markdown. You
 can read more about [Org mode exporting here.](http://orgmode.org/manual/Exporting.html)
 
-Exporting to Github Flavored Markdown is available through Org
+Exporting to Myst Markdown is available through Org
 mode's [export dispatcher](http://orgmode.org/manual/The-export-dispatcher.html#The-export-dispatcher)
-once `ox-gfm` is loaded. Alternatively, exporting can be triggered by calling the
-(autoloaded) function `M-x org-gfm-export-to-markdown`.
+once `ox-myst` is loaded. Alternatively, exporting can be triggered by calling the
+(autoloaded) function `M-x org-myst-export-to-markdown`.
 
-If you want to automatically load `ox-gfm` along with Org mode, then you can
+If you want to automatically load `ox-myst` along with Org mode, then you can
 add this snippet to your Emacs configuration:
 
 ```emacs-lisp
 (eval-after-load "org"
-  '(require 'ox-gfm nil t))
+  '(require 'ox-myst nil t))
 ```
--- ox-myst.el
+++ ox-myst.el
@@ -1,9 +1,9 @@
-;;; ox-gfm.el --- Github Flavored Markdown Back-End for Org Export Engine -*- lexical-binding: t; -*-
+;;; ox-myst.el --- Myst Markdown Back-End for Org Export Engine -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014-2017 Lars Tveito
+;; Copyright (C) 2014-2017 Lars Tveito, 2024 Matt Price
 
-;; Author: Lars Tveito
-;; Keywords: org, wp, markdown, github
+;; Author: Lars Tveito, Matt Price
+;; Keywords: org, wp, markdown, myst
 
 ;; This file is not part of GNU Emacs.
 
@@ -22,8 +22,8 @@
 
 ;;; Commentary:
 
-;; This library implements a Markdown back-end (github flavor) for Org
-;; exporter, based on the `md' back-end.
+;; This library implements a Markdown back-end (myst flavor) for Org
+;; exporter, based on the `md' back-end. Forked from the gfm backend.
 
 ;;; Code:
 
@@ -33,9 +33,9 @@
 
 ;;; User-Configurable Variables
 
-(defgroup org-export-gfm nil
+(defgroup org-export-myst nil
   "Options specific to Markdown export back-end."
-  :tag "Org Github Flavored Markdown"
+  :tag "Org Myst Markdown"
   :group 'org-export
   :version "24.4"
   :package-version '(Org . "8.0"))
@@ -43,32 +43,83 @@
 
 ;;; Define Back-End
 
-(org-export-define-derived-backend 'gfm 'md
+(org-export-define-derived-backend 'myst 'md
   :filters-alist '((:filter-parse-tree . org-md-separate-elements))
   :menu-entry
-  '(?g "Export to Github Flavored Markdown"
-       ((?G "To temporary buffer"
-            (lambda (a s v b) (org-gfm-export-as-markdown a s v)))
-        (?g "To file" (lambda (a s v b) (org-gfm-export-to-markdown a s v)))
-        (?o "To file and open"
+  '(?M "Export to Myst Markdown"
+       ((?m "To temporary buffer"
+            (lambda (a s v b) (org-myst-export-as-markdown a s v)))
+        (?M "To file" (lambda (a s v b) (org-myst-export-to-markdown a s v)))
+        (?O "To file and open"
             (lambda (a s v b)
-              (if a (org-gfm-export-to-markdown t s v)
-                (org-open-file (org-gfm-export-to-markdown nil s v)))))))
-  :translate-alist '((inner-template . org-gfm-inner-template)
-                     (paragraph . org-gfm-paragraph)
-                     (strike-through . org-gfm-strike-through)
-                     (example-block . org-gfm-example-block)
-                     (src-block . org-gfm-src-block)
-                     (table-cell . org-gfm-table-cell)
-                     (table-row . org-gfm-table-row)
-                     (table . org-gfm-table)))
+              (if a (org-myst-export-to-markdown t s v)
+                (org-open-file (org-myst-export-to-markdown nil s v)))))))
+  :translate-alist '((inner-template . org-myst-inner-template)
+                     (plain-text . org-myst-plain-text)
+                     (paragraph . org-myst-paragraph)
+                     (strike-through . org-myst-strike-through)
+                     (example-block . org-myst-example-block)
+                     (src-block . org-myst-src-block)
+                     (special-block . org-myst-special-block)
+                     (table-cell . org-myst-table-cell)
+                     (table-row . org-myst-table-row)
+                     (table . org-myst-table)))
 
 
 ;;; Transcode Functions
 
+;;; front-matter
+
+(defun org-myst-front-matter ()
+  "Add FRONT MATTER YAML to document.
+Currently this is not really a function, just passes the default
+YAML as a static string"
+  "---
+jupytext:
+  cell_metadata_filter: -all
+  formats: md:myst,ipynb,py:percent
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.16.1
+kernelspec:
+  display_name: Python 3 (ipykernel)
+  language: python
+  name: python3
+---
+")
+
+;;;; Plain Text
+
+(defun org-myst-plain-text (text info)
+  "Transcode a TEXT string into Markdown format.
+TEXT is the string to transcode.  INFO is a plist holding
+contextual information. Don't use smart quotes here."
+  ;; commenting this out is the reason this function exists
+  ;; (when (plist-get info :with-smart-quotes)
+  ;;   (setq text (org-export-activate-smart-quotes text :html info)))
+  ;; The below series of replacements in `text' is order sensitive.
+  ;; Protect `, *, _, and \
+  (setq text (replace-regexp-in-string "[`*_\\]" "\\\\\\&" text))
+  ;; Protect ambiguous #.  This will protect # at the beginning of
+  ;; a line, but not at the beginning of a paragraph.  See
+  ;; `org-md-paragraph'.
+  (setq text (replace-regexp-in-string "\n#" "\n\\\\#" text))
+  ;; Protect ambiguous !
+  (setq text (replace-regexp-in-string "\\(!\\)\\[" "\\\\!" text nil nil 1))
+  ;; Handle special strings, if required.
+  (when (plist-get info :with-special-strings)
+    (setq text (org-html-convert-special-strings text)))
+  ;; Handle break preservation, if required.
+  (when (plist-get info :preserve-breaks)
+    (setq text (replace-regexp-in-string "[ \t]*\n" "  \n" text)))
+  ;; Return value.
+  text)
+
 ;;;; Paragraph
 
-(defun org-gfm-paragraph (paragraph contents info)
+(defun org-myst-paragraph (paragraph contents info)
   "Transcode PARAGRAPH element into Github Flavoured Markdown format.
 CONTENTS is the paragraph contents.  INFO is a plist used as a
 communication channel."
@@ -82,24 +133,27 @@ communication channel."
 
 ;;;; Src Block
 
-(defun org-gfm-src-block (src-block _contents info)
-  "Transcode SRC-BLOCK element into Github Flavored Markdown format.
+(defun org-myst-src-block (src-block _contents info)
+  "Transcode SRC-BLOCK element into Myst Markdown format.
 _CONTENTS is nil.  INFO is a plist used as a communication
-channel."
+channel.  Temporarily setting notebook language statically"
   (let* ((lang (org-element-property :language src-block))
          (code (org-export-format-code-default src-block info))
-         (prefix (concat "```" lang "\n"))
+         (lang-fix (if (string-match-p "python" lang)
+                       "{code-cell}" lang))
+         (prefix (concat "```" lang-fix "\n"))
          (suffix "```"))
+    
     (concat prefix code suffix)))
 
 ;;;; Example Block
 
-(defalias 'org-gfm-example-block #'org-gfm-src-block)
+(defalias 'org-myst-example-block #'org-myst-src-block)
 
 ;;;; Strike-Through
 
-(defun org-gfm-strike-through (_strike-through contents _info)
-  "Transcode _STRIKE-THROUGH from Org to Markdown (GFM).
+(defun org-myst-strike-through (_strike-through contents _info)
+  "Transcode _STRIKE-THROUGH from Org to Markdown (MYST).
 CONTENTS is the text with strike-through markup.  _INFO is a plist
 holding contextual information."
   (format "~~%s~~" contents))
@@ -109,11 +163,11 @@ holding contextual information."
 (defvar width-cookies nil)
 (defvar width-cookies-table nil)
 
-(defconst gfm-table-left-border "|")
-(defconst gfm-table-right-border " |")
-(defconst gfm-table-separator " |")
+(defconst myst-table-left-border "|")
+(defconst myst-table-right-border " |")
+(defconst myst-table-separator " |")
 
-(defun org-gfm-table-col-width (table column info)
+(defun org-myst-table-col-width (table column info)
   "Return width of TABLE at given COLUMN.
 INFO is a plist used as communication channel.  Width of a column
 is determined either by inquerying `width-cookies' in the column,
@@ -146,24 +200,24 @@ or by the maximum cell with in the column."
             info)
           (puthash column max-width width-cookies))))))
 
-(defun org-gfm-make-hline-builder (table info char)
+(defun org-myst-make-hline-builder (table info char)
   "Return a function to build horizontal line in TABLE with given CHAR.
 INFO is a plist used as a communication channel."
   (lambda (col)
-    (let ((max-width (max 3 (org-gfm-table-col-width table col info))))
+    (let ((max-width (max 3 (org-myst-table-col-width table col info))))
       (when (< max-width 1)
         (setq max-width 1))
       (make-string max-width char))))
 
 ;;;; Table-Cell
 
-(defun org-gfm-table-cell (table-cell contents info)
-  "Transcode TABLE-CELL element from Org into GFM.
+(defun org-myst-table-cell (table-cell contents info)
+  "Transcode TABLE-CELL element from Org into MYST.
 CONTENTS is content of the cell.  INFO is a plist used as a
 communication channel."
   (let* ((table (org-export-get-parent-table table-cell))
          (column (cdr (org-export-table-cell-address table-cell info)))
-         (width (org-gfm-table-col-width table column info))
+         (width (org-myst-table-col-width table column info))
          (left-border (if (org-export-table-cell-starts-colgroup-p table-cell info) "| " " "))
          (right-border " |")
          (data (or contents "")))
@@ -175,31 +229,59 @@ communication channel."
 
 ;;;; Table-Row
 
-(defun org-gfm-table-row (table-row contents info)
-  "Transcode TABLE-ROW element from Org into GFM.
+(defun org-myst-table-row (table-row contents info)
+  "Transcode TABLE-ROW element from Org into MYST.
 CONTENTS is cell contents of TABLE-ROW.  INFO is a plist used as a
 communication channel."
   (let ((table (org-export-get-parent-table table-row)))
     (when (and (eq 'rule (org-element-property :type table-row))
-               ;; In GFM, rule is valid only at second row.
+               ;; In MYST, rule is valid only at second row.
                (eq 1 (cl-position
                       table-row
                       (org-element-map table 'table-row 'identity info))))
       (let* ((table (org-export-get-parent-table table-row))
-             (build-rule (org-gfm-make-hline-builder table info ?-))
+             (build-rule (org-myst-make-hline-builder table info ?-))
              (cols (cdr (org-export-table-dimensions table info))))
         (setq contents
-              (concat gfm-table-left-border
+              (concat myst-table-left-border
                       (mapconcat (lambda (col) (funcall build-rule col))
                                  (number-sequence 0 (- cols 1))
-                                 gfm-table-separator)
-                      gfm-table-right-border))))
+                                 myst-table-separator)
+                      myst-table-right-border))))
     contents))
+
+;;;; Special Block
+
+(defun org-myst-special-block (special-block contents info)
+  "Transcode a SPECIAL-BLOCK element from Org to Myst admonitions.
+CONTENTS holds the contents of the block.  INFO is a plist
+holding contextual information."
+  (let* ((block-type (org-element-property :type special-block))
+         (html5-fancy (and (org-html--html5-fancy-p info)
+                           (member block-type org-html-html5-elements)))
+         (attributes (org-export-read-attribute :attr_html special-block)))
+    (unless html5-fancy
+      (let ((admonition (plist-get attributes :admonition)))
+        (setq attributes (plist-put attributes :admonition
+                                    (if admonition (concat admonition " " block-type)
+                                      block-type)))))
+    (let* ((contents (or contents ""))
+	   (reference (org-html--reference special-block info))
+	   (a (org-html--make-attribute-string
+	       (if (or (not reference) (plist-member attributes :id))
+		   attributes
+		 (plist-put attributes :id reference))))
+	   (str (if (org-string-nw-p a) (concat " " a) "")))
+      (format ":::{%s}\n%s:::\n" block-type contents block-type)
+      ;; (if html5-fancy
+      ;;     (format "<%s%s>\n%s</%s>" block-type str contents block-type)
+      ;;   (format "<div%s>\n%s\n</div>" str contents))
+      )))
 
 ;;;; Table
 
-(defun org-gfm-table (table contents info)
-  "Transcode TABLE element into Github Flavored Markdown table.
+(defun org-myst-table (table contents info)
+  "Transcode TABLE element into Myst Markdown table.
 CONTENTS is the contents of the table.  INFO is a plist holding
 contextual information."
   (let* ((rows (org-element-map table 'table-row 'identity info))
@@ -207,24 +289,24 @@ contextual information."
          (cols (cdr (org-export-table-dimensions table info)))
          (build-dummy-header
           (lambda ()
-            (let ((build-empty-cell (org-gfm-make-hline-builder table info ?\s))
-                  (build-rule (org-gfm-make-hline-builder table info ?-))
+            (let ((build-empty-cell (org-myst-make-hline-builder table info ?\s))
+                  (build-rule (org-myst-make-hline-builder table info ?-))
                   (columns (number-sequence 0 (- cols 1))))
-              (concat gfm-table-left-border
+              (concat myst-table-left-border
                       (mapconcat (lambda (col) (funcall build-empty-cell col))
                                  columns
-                                 gfm-table-separator)
-                      gfm-table-right-border "\n" gfm-table-left-border
+                                 myst-table-separator)
+                      myst-table-right-border "\n" myst-table-left-border
                       (mapconcat (lambda (col) (funcall build-rule col))
                                  columns
-                                 gfm-table-separator)
-                      gfm-table-right-border "\n")))))
+                                 myst-table-separator)
+                      myst-table-right-border "\n")))))
     (concat (and no-header (funcall build-dummy-header))
             (replace-regexp-in-string "\n\n" "\n" contents))))
 
 ;;;; Table of contents
 
-(defun org-gfm-format-toc (headline info)
+(defun org-myst-format-toc (headline info)
   "Return an appropriate table of contents entry for HEADLINE."
   (let* ((title (org-export-data
                  (org-export-get-alt-title headline info) info))
@@ -236,7 +318,7 @@ contextual information."
 
 ;;;; Footnote section
 
-(defun org-gfm-footnote-section (info)
+(defun org-myst-footnote-section (info)
   "Format the footnote section.
 INFO is a plist used as a communication channel."
   (and-let* ((fn-alist (org-export-collect-footnote-definitions info)))
@@ -256,25 +338,26 @@ INFO is a plist used as a communication channel."
 
 ;;;; Template
 
-(defun org-gfm-inner-template (contents info)
+(defun org-myst-inner-template (contents info)
   "Return body of document after converting it to Markdown syntax.
 CONTENTS is the transcoded contents string.  INFO is a plist
 holding export options."
   (let* ((depth (plist-get info :with-toc))
          (headlines (and depth (org-export-collect-headlines info depth)))
          (toc-string (or (mapconcat (lambda (headline)
-                                      (org-gfm-format-toc headline info))
+                                      (org-myst-format-toc headline info))
                                     headlines "\n")
                          ""))
-         (toc-tail (if headlines "\n\n" "")))
-    (org-trim (concat toc-string toc-tail contents "\n" (org-gfm-footnote-section info)))))
+         (toc-tail (if headlines "\n\n" ""))
+         (front-matter (org-myst-front-matter)))
+    (org-trim (concat front-matter toc-string toc-tail contents "\n" (org-myst-footnote-section info)))))
 
 
 ;;; Interactive function
 
 ;;;###autoload
-(defun org-gfm-export-as-markdown (&optional async subtreep visible-only)
-  "Export current buffer to a Github Flavored Markdown buffer.
+(defun org-myst-export-as-markdown (&optional async subtreep visible-only)
+  "Export current buffer to a Myst Markdown buffer.
 
 If narrowing is active in the current buffer, only export its
 narrowed part.
@@ -292,26 +375,26 @@ first.
 When optional argument VISIBLE-ONLY is non-nil, don't export
 contents of hidden elements.
 
-Export is done in a buffer named \"*Org GFM Export*\", which will
+Export is done in a buffer named \"*Org MYST Export*\", which will
 be displayed when `org-export-show-temporary-export-buffer' is
 non-nil."
   (interactive)
-  (org-export-to-buffer 'gfm "*Org GFM Export*"
+  (org-export-to-buffer 'myst "*Org MYST Export*"
     async subtreep visible-only nil nil (lambda () (text-mode))))
 
 ;;;###autoload
-(defun org-gfm-convert-region-to-md ()
-  "Convert the region to Github Flavored Markdown.
+(defun org-myst-convert-region-to-md ()
+  "Convert the region to Myst Markdown.
 This can be used in any buffer, this function assume that the
 current region has org-mode syntax.  For example, you can write
 an itemized list in org-mode syntax in a Markdown buffer and use
 this command to convert it."
   (interactive)
-  (org-export-replace-region-by 'gfm))
+  (org-export-replace-region-by 'myst))
 
 ;;;###autoload
-(defun org-gfm-export-to-markdown (&optional async subtreep visible-only)
-  "Export current buffer to a Github Flavored Markdown file.
+(defun org-myst-export-to-markdown (&optional async subtreep visible-only)
+  "Export current buffer to a Myst Markdown file.
 
 If narrowing is active in the current buffer, only export its
 narrowed part.
@@ -332,17 +415,17 @@ contents of hidden elements.
 Return output file's name."
   (interactive)
   (let ((outfile (org-export-output-file-name ".md" subtreep)))
-    (org-export-to-file 'gfm outfile async subtreep visible-only)))
+    (org-export-to-file 'myst outfile async subtreep visible-only)))
 
 ;;;###autoload
-(defun org-gfm-publish-to-gfm (plist filename pub-dir)
+(defun org-myst-publish-to-myst (plist filename pub-dir)
   "Publish an org file to Markdown.
 FILENAME is the filename of the Org file to be published.  PLIST
 is the property list for the given project.  PUB-DIR is the
 publishing directory.
 Return output file name."
-  (org-publish-org-to 'gfm filename ".md" plist pub-dir))
+  (org-publish-org-to 'myst filename ".md" plist pub-dir))
 
-(provide 'ox-gfm)
+(provide 'ox-myst)
 
-;;; ox-gfm.el ends here
+;;; ox-myst.el ends here
